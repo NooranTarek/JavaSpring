4b37644a065e920b09e4a5915c167ee901a5ec3c
NooranTarek <noort.mohamed@gmail.com>
2024-11-08 15:55:15 +0200

    accessing contains methos in view

--- demo/src/main/java/com/example/demo/StaffController.java
+++ demo/src/main/java/com/example/demo/StaffController.java
@@ -17,6 +17,7 @@ public class StaffController {
         //add an object named staffData to the model
         //This name (staffData) will act as a key to reference the (myStaff) object in the view
         model.addAttribute("staffData", myStaff);
+        model.addAttribute("str", "i LOVE java");
         //using View Resolver(Thymeleaf template engine) to know the file of this name
         return "staffDetails";
     }
