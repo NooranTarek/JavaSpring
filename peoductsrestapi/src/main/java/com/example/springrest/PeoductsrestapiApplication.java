e4b3f1488255d731eb0973791af6f56382b8c309
NooranTarek <noort.mohamed@gmail.com>
2024-11-11 12:56:59 +0200

    adding rest api products app

---
+++ peoductsrestapi/src/main/java/com/example/springrest/PeoductsrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoductsrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoductsrestapiApplication.class, args);
+	}
+
+}
---
+++ peoductsrestapi/src/test/java/com/example/springrest/PeoductsrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoductsrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
