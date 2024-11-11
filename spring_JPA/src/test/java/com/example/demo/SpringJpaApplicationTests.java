9fb0b943e9f5a04ac4e828e4d61a1056c8d1cb20
NooranTarek <noort.mohamed@gmail.com>
2024-11-11 12:44:06 +0200

    testing simple app

--- spring_JPA/src/test/java/com/example/demo/SpringJpaApplicationTests.java
+++ spring_JPA/src/test/java/com/example/demo/SpringJpaApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Player;
+import com.example.demo.model.repo.PlayerRepository;
 
 @SpringBootTest
 class SpringJpaApplicationTests {
 
+	@Autowired
+	private PlayerRepository repository;
+	
 	@Test
-	void contextLoads() {
+	public void testSavePlayer() {
+		Player player=new Player();
+		player.setId(1l);
+		player.setName("nour");
+		player.setScore(20);
+		repository.save(player);
+		Player savedPlayer=repository.findById(1l).get();
+		assertNotNull(savedPlayer);
+		
 	}
 
 }
