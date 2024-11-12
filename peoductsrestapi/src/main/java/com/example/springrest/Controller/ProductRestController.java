514abf495ad56529204db42c428ca8e53b569dcd
NooranTarek <noort.mohamed@gmail.com>
2024-11-12 22:05:32 +0200

    adding response with message and use logger via lambok

--- peoductsrestapi/src/main/java/com/example/springrest/Controller/ProductRestController.java
+++ peoductsrestapi/src/main/java/com/example/springrest/Controller/ProductRestController.java
@@ -2,6 +2,7 @@ package com.example.springrest.Controller;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,34 +10,46 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springrest.ApiResponse.ApiResponse;
 import com.example.springrest.entities.Product;
 import com.example.springrest.repos.ProductRepository;
 
+
+
 @RestController
+@Slf4j
 public class ProductRestController {
 
 	@Autowired
 	ProductRepository repository;
 	
-	@GetMapping("/products/")
+	@GetMapping("/")
 	public List<Product> getProducts(){
 		return repository.findAll();
 	}
-	@GetMapping("/products/{id}")
+	@GetMapping("/{id}")
 	public Product getProductById(@PathVariable("id") int id ){
 		return repository.findById(id).get();
 	}
-	@PostMapping("/products/")
-	public Product addProduct(@RequestBody Product product) {
-		return repository.save(product);
+	@PostMapping("/")
+	@ResponseBody 
+	public ApiResponse<Product> addProduct(@RequestBody Product product) {
+	    Product savedProduct = repository.save(product);
+//	    System.out.println("Product saved: " + savedProduct.getName());
+//	    System.out.println("Message: Product added successfully!");
+		log.info("Product saved: {}", savedProduct.getName());
+		log.info("Message: Product added successfully!");
+	    return new ApiResponse<>("Product added successfully!", savedProduct);
 	}
-	@PutMapping("/products/")
+
+	@PutMapping("/")
 	public Product updateProduct (@RequestBody Product product) {
 		return repository.save(product);
 	}
-	@DeleteMapping("/products/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteProduct (@PathVariable ("id") int id ) {
 		repository.deleteById(id);
 	}
